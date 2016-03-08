714789236f396b4d8087d7acaaa881964c121d5d
deeiip <ping@dipanjan.me>
2016-03-09 01:45:49 +0530

    fake

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -5,6 +5,7 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../src/Surrounding.c \
+../src/forwarding.c \
 ../src/interfaces.c \
 ../src/listening.c \
 ../src/log.c \
@@ -12,6 +13,7 @@ C_SRCS += \
 
 OBJS += \
 ./src/Surrounding.o \
+./src/forwarding.o \
 ./src/interfaces.o \
 ./src/listening.o \
 ./src/log.o \
@@ -19,6 +21,7 @@ OBJS += \
 
 C_DEPS += \
 ./src/Surrounding.d \
+./src/forwarding.d \
 ./src/interfaces.d \
 ./src/listening.d \
 ./src/log.d \
@@ -29,7 +32,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/dipanjan/workspace/Zuko/include" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -Iinclude -I"/home/dipanjan/workspace/Zuko/include" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
